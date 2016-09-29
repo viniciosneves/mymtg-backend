f8ded85dc9ef1b17980ca9fdb78b70f7ff288795
Marcus Balbi <balbimarcus@gmail.com>
2016-09-28 22:36:36 -0300

    changes on IndexRequest

--- app/Common/Http/Requests/IndexRequest.php
+++ app/Common/Http/Requests/IndexRequest.php
@@ -2,21 +2,17 @@
 
 namespace Balbi\MyMtg\Common\Http\Requests;
 
-class IndexRequest extends Request
-{
+class IndexRequest extends Request {
 
-    public function authorize()
-    {
-       return true;
-    }
+	public function authorize() {
+		return true;
+	}
 
-    public function rules()
-    {
-      return [];
-    }
+	public function rules() {
+		return [];
+	}
 
-    public function perPage()
-    {
-        return $this->get('per_page',15);
-    }
+	public function perPage() {
+		return $this->get('per_page', 15);
+	}
 }
